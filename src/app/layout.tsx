1d3c9f74f6f6e1dda487f01044b2ef46be4b8278
Arturo Legaspi Rodrigo <artuleg@gmail.com>
2025-08-05 13:12:01 +0200

    Google Analitics Added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { GoogleTagManager } from '@next/third-parties/google'
+import { GoogleAnalytics, GoogleTagManager } from '@next/third-parties/google'
 
 const inter = Inter({
   variable: "--font-inter",
@@ -39,6 +39,7 @@ export default function RootLayout({
   return (
     <html lang="es" className="scroll-smooth">
       <GoogleTagManager gtmId="G-LCBZRG437T" />
+      <GoogleAnalytics gaId="G-LCBZRG437T" />
       <body
         className={`${inter.variable} font-sans antialiased bg-[#0a0a0a] text-gray-200`}
       >
